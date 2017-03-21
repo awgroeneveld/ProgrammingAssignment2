2dd767731093bd332994e3128133302cc9029def
Aart-Wichert Groeneveld <awgroeneveld@gmail.com>
2017-03-21 19:11:49 +0100

    Give the functions some body

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a matrix with caching properties
+## basicMatrix = matrix to base the CacheMatrix upon; defaults to empty matrix
+makeCacheMatrix <- function(basicMatrix = matrix()) {
+    inverseMatrix <- NULL
+    
+    # sets matrix data
+    set <- function(matrix) {
+      basicMatrix <<- matrix
+      inverseMatrix <<- NULL
+    }
+    
+    # gets matrix data
+    get <- function() basicMatrix
+    
+    # sets the inverse matri x
+    setinverse <- function(inverse) inverseMatrix <<- inverse
+    
+    # returns inverseMatrix
+    getinverse <- function() inverseMatrix
+    
+    # set of functions within scope
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function calculates the inverse of the matrix x or returns the cached
+## value if already calculated.
+cacheSolve <- function(enhancedMatrix, ...) {
+  # first find out if it has been cached
+  inverseMatrix <- enhancedMatrix$getinverse()
+  if(!is.null(inverseMatrix)) {
+    message("getting cached data")
+    return(inverseMatrix)
+  }
+  
+  # so we need a calculation
+  # extract the base matrix and take its inverse
+  matrixData <- enhancedMatrix$get()
+  inverseMatrix <- solve(matrixData,...)
+  
+  # sets the value into cache
+  enhancedMatrix$setinverse(inverseMatrix)
+  
+  #return the obtained value
+  inverseMatrix
 }
+
